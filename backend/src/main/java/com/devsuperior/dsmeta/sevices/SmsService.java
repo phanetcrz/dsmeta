b55de580ead6f90cf32679d05225476604b16e2b
phanetcrz <phanet@gmail.com>
2022-07-15 21:17:07 -0300

    Axios, useEffect first request

--- backend/src/main/java/com/devsuperior/dsmeta/sevices/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/sevices/SmsService.java
@@ -34,6 +34,8 @@ public class SmsService {
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear(); 
 		
 		String msg = "Vendedor "+ sale.getSellerName() + " foi destaque em " + date
+				//+ " com um total de R$ " + String.format("%.0f", sale.getAmount());
+		 		//+ " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
 				+ " com um total de R$ " + String.format("%.0f", sale.getAmount());
 		
 		Twilio.init(twilioSid, twilioKey);
